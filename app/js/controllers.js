66f62c315acf824523ab614338b155aa98548945
Oscar Moreno Garza <oscarmg99@gmail.com>
2014-04-27 04:09:05 -0500

    modified:   app/js/controllers.js 	shows error, if it cant connect to a database.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,9 +5,17 @@ var microBlogControllers = angular.module('microBlog.controllers', [])
 
 /*load all the posts here*/
 microBlogControllers.controller('DefaultCtrl', ['$scope', '$http', function($scope, $http) {
+
     $http.get('/api/posts').success(function(data) {
         $scope.posts = data.slice().reverse();
     });
+
+    if (! $scope.posts ) {
+        $scope.posts = [{
+                    titulo: "Error en conexion a la DB",
+                    post: "No me puedo conectar al servidor de base de datos" }];
+    }
+
 }]);
 
 
@@ -27,7 +35,7 @@ microBlogControllers.controller('CreatePostCtrl', ['$scope', '$http', function($
             titulo: $scope.titulo,
             post: $scope.post
         }).success( function() {
-            console.log("Datos actualizados con exito");
+            /*redirect to home ?*/
         });
     };
 
